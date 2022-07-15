f56b79fffb15580660ff0cc292b816d4183c15a0
Nicolas Lo Vento <nicolaslovento.94@hotmail.com>
2022-07-15 20:31:00 -0300

    avance header

--- src/views/Header/index.js
+++ src/views/Header/index.js
@@ -1,10 +1,11 @@
 import './style.css'
-
+import logo from '../../assets/images/logo.jpg'
 export default function Header() {
   return (
     <div className="header">
       <div className="header__logo">
-
+        <img src={logo} alt="logo" />
+        <span>COMPLEJO DEPORTIVO</span>
       </div>
       <div className="header__info">
 
--- src/views/Menu/index.js
+++ src/views/Menu/index.js
@@ -1,27 +1,20 @@
 import './style.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFutbol, faBaseball,faBasketballBall} from '@fortawesome/free-solid-svg-icons'
 export default function Menu() {
   return (
     <div className="menu">
       <div className="menu__item">
-        <div className="menu__item-icon">icon</div>
-        <div className="menu__item-title">title</div>
+        <div className="menu__item-icon"><FontAwesomeIcon icon={faFutbol} /></div>
+        <div className="menu__item-title">FÚTBOL</div>
       </div>
       <div className="menu__item">
-        <div className="menu__item-icon">icon</div>
-        <div className="menu__item-title">title</div>
+        <div className="menu__item-icon"><FontAwesomeIcon icon={faBaseball} /></div>
+        <div className="menu__item-title">TENIS</div>
       </div>
       <div className="menu__item">
-        <div className="menu__item-icon">icon</div>
-        <div className="menu__item-title">title</div>
-      </div>
-      <div className="menu__item">
-        <div className="menu__item-icon">icon</div>
-        <div className="menu__item-title">title</div>
-      </div>
-      <div className="menu__item">
-        <div className="menu__item-icon">icon</div>
-        <div className="menu__item-title">title</div>
+        <div className="menu__item-icon"><FontAwesomeIcon icon={faBasketballBall} /></div>
+        <div className="menu__item-title">BASCKET</div>
       </div>
     </div>
   );
