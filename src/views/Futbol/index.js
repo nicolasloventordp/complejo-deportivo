4ba62e792b902a7a77963a542f179452b88718fd
Nicolas Lo Vento <nicolaslovento.94@gmail.com>
2022-07-19 18:01:45 -0300

    futbol: avance

--- src/views/Futbol/index.js
+++ src/views/Futbol/index.js
@@ -1,9 +1,21 @@
 import './style.css'
+import imagenCancha from '../../assets/images/home-canchas.jpg'
 
 export default function Futbol() {
   return (
     <div className="futbol">
-      <h1>Seccion Futbol</h1>
+      <div className="futbol-container">
+        <div className="futbol__card">
+          <img className="futbol__card-img" src={imagenCancha} />
+          <div className="futbol__card-info">
+            <div>
+              <span>Caracteristicas</span>
+              <span>Servicios</span>
+            </div>
+            <button>RESERVAR</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
